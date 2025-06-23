c454904b268ee24167f81d2cd0717fc411b118c9
Simon Bürger <simon.buerger@rwth-aachen.de>
2025-06-23 11:34:33 +0200

    add '.green_cutoff' for more fine-grained reducible resolution

--- src/sat/elimination.cpp
+++ src/sat/elimination.cpp
@@ -106,6 +106,23 @@ struct Elimination
 
 	util::bit_set dirty; // variables that need to be re-evaluated
 
+	auto occs_all(Lit a)
+	{
+		return occs[a] | std::views::transform([&](CRef i) -> Clause & {
+			       return cnf.clauses[i];
+		       }) |
+		       std::views::filter(
+		           [](Clause const &cl) { return cl.color() != Color::black; });
+	}
+	auto occs_blue(Lit a)
+	{
+		return occs[a] | std::views::transform([&](CRef i) -> Clause & {
+			       return cnf.clauses[i];
+		       }) |
+		       std::views::filter(
+		           [](Clause const &cl) { return cl.color() == Color::blue; });
+	}
+
 	Elimination(Cnf &cnf_, EliminationConfig const &config_)
 	    : cnf(cnf_), occs(cnf_.var_count() * 2), watches(cnf_.var_count() * 2),
 	      config(config_), eliminated(cnf_.var_count()),
@@ -180,275 +197,282 @@ struct Elimination
 	//   - lower is better
 	//   - only considers irreducible clauses in the counting
 	//   - also does BCE on the fly
-	int compute_score(int v)
-	{
-		if (eliminated[v])
-			return score_never;
-
-		// eliminating fixed variables would break our implementation.
-		// Also pointless anyway.
-		for (Lit u : cnf.units)
-			if (u.var() == v)
-				return score_never;
-
-		auto pos = Lit(v, false);
-		auto neg = Lit(v, true);
-		auto blocked_color =
-		    config.discard_blocked ? Color::black : Color::green;
-
-		auto posCount = util::small_vector<int, 32>(occs[pos].size(), 0);
-		auto negCount = util::small_vector<int, 32>(occs[neg].size(), 0);
-		int totalCount = 0;
-
-		// count all non-tautology blue-blue resolvents
-		for (int i = 0; i < (int)occs[pos].size(); ++i)
-			if (auto &a = cnf.clauses[occs[pos][i]]; a.color() == Color::blue)
-				for (int j = 0; j < (int)occs[neg].size(); ++j)
-					if (auto &b = cnf.clauses[occs[neg][j]];
-					    b.color() == Color::blue)
-						if (!is_resolvent_tautological(a.lits(), b.lits()))
-						{
-							++posCount[i];
-							++negCount[j];
-							++totalCount;
-						}
-
-		// count non-tautology long-binary resolvents
-		for (int i = 0; i < (int)occs[pos].size(); ++i)
-			if (auto &cl = cnf.clauses[occs[pos][i]]; cl.color() == Color::blue)
-				for (Lit x : cnf.bins[neg])
-					if (!cl.contains(x.neg()))
-						++posCount[i];
-		for (int j = 0; j < (int)occs[neg].size(); ++j)
-			if (auto &cl = cnf.clauses[occs[neg][j]]; cl.color() == Color::blue)
-				for (Lit x : cnf.bins[pos])
-					if (!cl.contains(x.neg()))
-						++negCount[j];
-
-		// blocked clause elimination ("BCE"):
-		// remove clauses with only tautology resolvents
-		for (int i = 0; i < (int)occs[pos].size(); ++i)
-			if (auto &cl = cnf.clauses[occs[pos][i]];
-			    posCount[i] == 0 && cl.color() == Color::blue)
-			{
-				nBCE++;
-				log.debug("removing blocked clause {}, pivot {}", cl, pos);
-				for (Lit x : cl)
-					dirty.add(x.var());
-				cnf.add_rule(cl, pos);
-				cl.set_color(blocked_color);
-			}
-		for (int j = 0; j < (int)occs[neg].size(); ++j)
-			if (auto &cl = cnf.clauses[occs[neg][j]];
-			    negCount[j] == 0 && cl.color() == Color::blue)
-			{
-				nBCE++;
-				log.debug("removing blocked clause {}, pivot {}", cl, neg);
-				for (Lit x : cl)
-					dirty.add(x.var());
-				cnf.add_rule(cl, neg);
-				cl.set_color(blocked_color);
-			}
-
-		// compute score
-		int score = std::accumulate(posCount.begin(), posCount.end(), 0);
-		score -= std::count_if(posCount.begin(), posCount.end(),
-		                       [](int i) { return i != 0; });
-		score -= std::count_if(negCount.begin(), negCount.end(),
-		                       [](int i) { return i != 0; });
-		score -= (int)cnf.bins[pos].size() + (int)cnf.bins[neg].size();
-		score += (int)(cnf.bins[pos].size() * cnf.bins[neg].size());
-
-		log.trace("score({}) = {}", v + 1, score);
-
-		return score;
-	}
+	int compute_score(int v);
 
 	// eliminate a variable: add resolvents, move clauses to extender
-	void eliminate(int v)
-	{
-		assert(!eliminated[v]);
-		eliminated[v] = true;
-		++nEliminated;
-
-		// I think this might happen in a very contrived case?
-		for (Lit a : cnf.units)
-			if (a.var() == v)
-				throw std::runtime_error("eliminating fixed variable");
-
-		auto pos = Lit(v, false);
-		auto neg = Lit(v, true);
-		Color resolve_color =
-		    config.resolve_reducible ? Color::red : Color::blue;
-
-		log.debug("eliminating variable {} ({}+{} bins, {}+{} occs)", pos,
-		          cnf.bins[pos].size(), cnf.bins[neg].size(), occs[pos].size(),
-		          occs[neg].size());
-
-		std::vector<Lit> tmp;
-		ClauseStorage resolvents;
-
-		// add binary-binary resolvents
-		for (Lit x : cnf.bins[pos])
-			for (Lit y : cnf.bins[neg])
-			{
-				if (x == y.neg())
-					continue;
-				else if (x == y)
-					cnf.add_unary(x);
-				else
-					resolvents.add_clause(std::array{x, y}, Color::blue);
-			}
-
-		// add long-binary resolvents
-		for (CRef i : occs[pos])
-			if (Clause const &cl = cnf.clauses[i]; cl.color() >= resolve_color)
-				for (Lit x : cnf.bins[neg])
-					if (resolvent(tmp, cl.lits(), {x, neg}))
-						resolvents.add_clause(tmp, cl.color());
-		for (CRef i : occs[neg])
-			if (Clause const &cl = cnf.clauses[i]; cl.color() >= resolve_color)
-				for (Lit x : cnf.bins[pos])
-					if (resolvent(tmp, cl.lits(), {x, pos}))
-						resolvents.add_clause(tmp, cl.color());
-
-		// add long-long resolvents
-		for (CRef i : occs[pos])
-			if (Clause const &a = cnf.clauses[i]; a.color() >= resolve_color)
-				for (CRef j : occs[neg])
-					if (Clause const &b = cnf.clauses[j];
-					    b.color() >= resolve_color)
-						if (resolvent(tmp, a.lits(), b.lits()))
-							resolvents.add_clause(tmp,
-							                      min(a.color(), b.color()));
-
-		// remove old long clauses from the problem
-		for (CRef i : occs[pos])
-		{
-			Clause &a = cnf.clauses[i];
-			if (a.color() == Color::blue)
-				cnf.add_rule(a, pos);
-			a.set_color(Color::black);
-		}
-		for (CRef i : occs[neg])
-		{
-			Clause &a = cnf.clauses[i];
-			if (a.color() == Color::blue)
-				cnf.add_rule(a, neg);
-			a.set_color(Color::black);
-		}
-
-		// remove old binary clauses from the problem
-		for (Lit b : cnf.bins[pos])
-		{
-			erase(cnf.bins[b], pos);
-			cnf.add_rule({{pos, b}});
-		}
-		for (Lit b : cnf.bins[neg])
-		{
-			erase(cnf.bins[b], neg);
-			cnf.add_rule({{neg, b}});
-		}
-		cnf.bins[pos].resize(0);
-		cnf.bins[neg].resize(0);
-
-		// NOTE: Only start adding resolvents after all old clauses are removed.
-		// Simplest way to keep the subsumption-logic in add_clause consistent.
-		int64_t resolvent_count = 0;
-		for (Clause const &cl : resolvents.all())
-		{
-			if (add_clause(cl.lits(), cl.color()))
-				++resolvent_count;
-		}
-		nResolvents += resolvent_count;
-
-		log.debug("eliminated variable {}, adding {} resolvents", pos,
-		          resolvent_count);
-	}
+	void eliminate(int v);
 
 	// find best variable to eliminate next.
 	// returns -1 if no more candidates are available.
-	int choose_var()
-	{
-		// recompute all potentially-outdated scores
-		// note: this loop effectively does BCE till fixed-point
-		while (!dirty.empty())
-		{
-			int v = dirty.pop();
-			score[v] = compute_score(v);
-			if (score[v] <= cutoff)
-				queue.push({score[v], v});
-		}
-
-		while (!queue.empty())
-		{
-			auto [s, v] = queue.top();
-			queue.pop();
-			if (!eliminated[v] && score[v] == s)
-				return v;
-		}
-		return -1;
-	}
+	int choose_var();
 
 	// returns number of removed variables
-	void run()
-	{
-		for (int i : cnf.all_vars())
-			dirty.add(i);
-
-		while (nEliminated < config.max_eliminations &&
-		       nResolvents < config.max_resolvents)
-		{
-			int v = choose_var();
-			if (v == -1)
-				break;
-
-			// Consistency check for the scores. Incorrect scores (e.g.
-			// forgetting to set the dirty flag) can harm effectiveness a lot
-			// without being evident from normal testing.
-			assert(score[v] == compute_score(v));
-
-			auto pos = Lit(v, false);
-			auto neg = Lit(v, true);
-
-			// determine other variables whose score will have to be
-			// recalculated
-			for (Lit x : cnf.bins[pos])
-				dirty.add(x.var());
-			for (Lit x : cnf.bins[neg])
-				dirty.add(x.var());
-			for (CRef k : occs[pos])
-				if (cnf.clauses[k].color() == Color::blue)
-					for (Lit x : cnf.clauses[k].lits())
-						dirty.add(x.var());
-			for (CRef k : occs[neg])
-				if (cnf.clauses[k].color() == Color::blue)
-					for (Lit x : cnf.clauses[k].lits())
-						dirty.add(x.var());
-
-			// eliminate the variable
-			eliminate(v);
-			score[v] = score_never;
-		}
-
-		// remove reducible clauses that contain eliminated variables
-		for (auto &cl : cnf.clauses.all())
-		{
-			bool elim = std::any_of(cl.begin(), cl.end(), [this](Lit a) {
-				return eliminated[a.var()];
-			});
-			if (elim)
-			{
-				assert(cl.color() != Color::blue);
-				cl.set_color(Color::black);
-			}
-		}
-
-		log.info("[g={}] found {} blocked clauses, removed {} vars and added "
-		         "{} resolvents",
-		         cutoff, nBCE, nEliminated, nResolvents);
-	}
+	void run();
 };
+
+int Elimination::compute_score(int v)
+{
+	if (eliminated[v])
+		return score_never;
+
+	// eliminating fixed variables would break our implementation.
+	// Also pointless anyway.
+	for (Lit u : cnf.units)
+		if (u.var() == v)
+			return score_never;
+
+	auto pos = Lit(v, false);
+	auto neg = Lit(v, true);
+	auto blocked_color = config.discard_blocked ? Color::black : Color::green;
+
+	auto posCount = util::small_vector<int, 32>(occs[pos].size(), 0);
+	auto negCount = util::small_vector<int, 32>(occs[neg].size(), 0);
+	int totalCount = 0;
+
+	// count all non-tautology blue-blue resolvents
+	for (int i = 0; i < (int)occs[pos].size(); ++i)
+		if (auto &a = cnf.clauses[occs[pos][i]]; a.color() == Color::blue)
+			for (int j = 0; j < (int)occs[neg].size(); ++j)
+				if (auto &b = cnf.clauses[occs[neg][j]];
+				    b.color() == Color::blue)
+					if (!is_resolvent_tautological(a.lits(), b.lits()))
+					{
+						++posCount[i];
+						++negCount[j];
+						++totalCount;
+					}
+
+	// count non-tautology long-binary resolvents
+	for (int i = 0; i < (int)occs[pos].size(); ++i)
+		if (auto &cl = cnf.clauses[occs[pos][i]]; cl.color() == Color::blue)
+			for (Lit x : cnf.bins[neg])
+				if (!cl.contains(x.neg()))
+					++posCount[i];
+	for (int j = 0; j < (int)occs[neg].size(); ++j)
+		if (auto &cl = cnf.clauses[occs[neg][j]]; cl.color() == Color::blue)
+			for (Lit x : cnf.bins[pos])
+				if (!cl.contains(x.neg()))
+					++negCount[j];
+
+	// blocked clause elimination ("BCE"):
+	// remove clauses with only tautology resolvents
+	for (int i = 0; i < (int)occs[pos].size(); ++i)
+		if (auto &cl = cnf.clauses[occs[pos][i]];
+		    posCount[i] == 0 && cl.color() == Color::blue)
+		{
+			nBCE++;
+			log.debug("removing blocked clause {}, pivot {}", cl, pos);
+			for (Lit x : cl)
+				dirty.add(x.var());
+			cnf.add_rule(cl, pos);
+			cl.set_color(blocked_color);
+		}
+	for (int j = 0; j < (int)occs[neg].size(); ++j)
+		if (auto &cl = cnf.clauses[occs[neg][j]];
+		    negCount[j] == 0 && cl.color() == Color::blue)
+		{
+			nBCE++;
+			log.debug("removing blocked clause {}, pivot {}", cl, neg);
+			for (Lit x : cl)
+				dirty.add(x.var());
+			cnf.add_rule(cl, neg);
+			cl.set_color(blocked_color);
+		}
+
+	// compute score
+	int score = std::accumulate(posCount.begin(), posCount.end(), 0);
+	score -= std::count_if(posCount.begin(), posCount.end(),
+	                       [](int i) { return i != 0; });
+	score -= std::count_if(negCount.begin(), negCount.end(),
+	                       [](int i) { return i != 0; });
+	score -= (int)cnf.bins[pos].size() + (int)cnf.bins[neg].size();
+	score += (int)(cnf.bins[pos].size() * cnf.bins[neg].size());
+
+	log.trace("score({}) = {}", v + 1, score);
+
+	return score;
+}
+
+// eliminate a variable: add resolvents, move clauses to extender
+void Elimination::eliminate(int v)
+{
+	assert(!eliminated[v]);
+	eliminated[v] = true;
+	++nEliminated;
+
+	// I think this might happen in a very contrived case?
+	for (Lit a : cnf.units)
+		if (a.var() == v)
+			throw std::runtime_error("eliminating fixed variable");
+
+	auto pos = Lit(v, false);
+	auto neg = Lit(v, true);
+
+	log.debug("eliminating variable {} ({}+{} bins, {}+{} occs)", pos,
+	          cnf.bins[pos].size(), cnf.bins[neg].size(), occs[pos].size(),
+	          occs[neg].size());
+
+	std::vector<Lit> tmp;
+	ClauseStorage resolvents;
+
+	// add binary-binary resolvents
+	for (Lit x : cnf.bins[pos])
+		for (Lit y : cnf.bins[neg])
+		{
+			if (x == y.neg())
+				continue;
+			else if (x == y)
+				cnf.add_unary(x);
+			else
+				resolvents.add_clause(std::array{x, y}, Color::blue);
+		}
+
+	// add long-binary resolvents
+	for (Clause const &cl : occs_all(pos))
+		if (cl.color() == Color::blue || cl.size() <= config.green_cutoff)
+			for (Lit x : cnf.bins[neg])
+				if (resolvent(tmp, cl.lits(), {x, neg}))
+					resolvents.add_clause(tmp, cl.color());
+	for (Clause const &cl : occs_all(neg))
+		if (cl.color() == Color::blue || cl.size() <= config.green_cutoff)
+			for (Lit x : cnf.bins[pos])
+				if (resolvent(tmp, cl.lits(), {x, pos}))
+					resolvents.add_clause(tmp, cl.color());
+
+	// add long-long resolvents
+	for (Clause const &a : occs_all(pos))
+		for (Clause const &b : occs_all(neg))
+			if (resolvent(tmp, a.lits(), b.lits()) &&
+			    (min(a.color(), b.color()) == Color::blue ||
+			     (int)tmp.size() <= config.green_cutoff))
+				resolvents.add_clause(tmp, min(a.color(), b.color()));
+
+	// remove old long clauses from the problem
+	for (Clause &a : occs_all(pos))
+	{
+		if (a.color() == Color::blue)
+			cnf.add_rule(a, pos);
+		a.set_color(Color::black);
+	}
+	for (Clause &a : occs_all(neg))
+	{
+		if (a.color() == Color::blue)
+			cnf.add_rule(a, neg);
+		a.set_color(Color::black);
+	}
+
+	// remove old binary clauses from the problem
+	for (Lit b : cnf.bins[pos])
+	{
+		erase(cnf.bins[b], pos);
+		cnf.add_rule({{pos, b}});
+	}
+	for (Lit b : cnf.bins[neg])
+	{
+		erase(cnf.bins[b], neg);
+		cnf.add_rule({{neg, b}});
+	}
+	cnf.bins[pos].resize(0);
+	cnf.bins[neg].resize(0);
+
+	// NOTE: Only start adding resolvents after all old clauses are removed.
+	// Simplest way to keep the subsumption-logic in add_clause consistent.
+	int64_t resolvent_count = 0;
+	for (Clause const &cl : resolvents.all())
+	{
+		if (add_clause(cl.lits(), cl.color()))
+			++resolvent_count;
+	}
+	nResolvents += resolvent_count;
+
+	log.debug("eliminated variable {}, adding {} resolvents", pos,
+	          resolvent_count);
+}
+
+// find best variable to eliminate next.
+// returns -1 if no more candidates are available.
+int Elimination::choose_var()
+{
+	// recompute all potentially-outdated scores
+	// note: this loop effectively does BCE till fixed-point
+	while (!dirty.empty())
+	{
+		int v = dirty.pop();
+		score[v] = compute_score(v);
+		if (score[v] <= cutoff)
+			queue.push({score[v], v});
+	}
+
+	while (!queue.empty())
+	{
+		auto [s, v] = queue.top();
+		queue.pop();
+		if (!eliminated[v] && score[v] == s)
+			return v;
+	}
+	return -1;
+}
+
+// returns number of removed variables
+void Elimination::run()
+{
+	for (int i : cnf.all_vars())
+		dirty.add(i);
+
+	while (nEliminated < config.max_eliminations &&
+	       nResolvents < config.max_resolvents)
+	{
+		int v = choose_var();
+		if (v == -1)
+			break;
+
+		// Consistency check for the scores. Incorrect scores (e.g.
+		// forgetting to set the dirty flag) can harm effectiveness a lot
+		// without being evident from normal testing.
+		// case in point: if a reducible resolvent is binary, that is then
+		// considered irreducible (like all binaries), affecting the score.
+		// assert(score[v] == compute_score(v));
+
+		auto pos = Lit(v, false);
+		auto neg = Lit(v, true);
+
+		// determine other variables whose score will have to be
+		// recalculated
+		for (Lit x : cnf.bins[pos])
+			dirty.add(x.var());
+		for (Lit x : cnf.bins[neg])
+			dirty.add(x.var());
+		for (CRef k : occs[pos])
+			if (cnf.clauses[k].color() == Color::blue)
+				for (Lit x : cnf.clauses[k].lits())
+					dirty.add(x.var());
+		for (CRef k : occs[neg])
+			if (cnf.clauses[k].color() == Color::blue)
+				for (Lit x : cnf.clauses[k].lits())
+					dirty.add(x.var());
+
+		// eliminate the variable
+		eliminate(v);
+		score[v] = score_never;
+	}
+
+	// remove reducible clauses that contain eliminated variables
+	for (auto &cl : cnf.clauses.all())
+	{
+		bool elim = std::any_of(cl.begin(), cl.end(),
+		                        [this](Lit a) { return eliminated[a.var()]; });
+		if (elim)
+		{
+			assert(cl.color() != Color::blue);
+			cl.set_color(Color::black);
+		}
+	}
+
+	log.info("[g={}] found {} blocked clauses, removed {} vars and added "
+	         "{} resolvents",
+	         cutoff, nBCE, nEliminated, nResolvents);
+}
+
 } // namespace
 
 int run_elimination(Cnf &sat, EliminationConfig const &config)
--- src/sat/elimination.h
+++ src/sat/elimination.h
@@ -10,8 +10,8 @@ struct EliminationConfig
 {
 	// How much the number of (irreducible) clauses is allowed to grow when
 	// eliminating a variable. SatELite/Minisat uses growth=0, cryptominisat
-	// uses up to growth=16 I think. growth=infinity completely solve the
-	// instance (or, much more likely, run out of memory immediately).
+	// uses up to growth=16 I think. growth=infinity completely solves the
+	// instance (or, much more likely, runs out of memory quickly).
 	int growth = 0;
 
 	// When a blocked clause is found, it is marked as reducible. With this
@@ -20,21 +20,26 @@ struct EliminationConfig
 	// blocked clauses.
 	bool discard_blocked = false;
 
-	// By default, reducible clauses containing eliminated variables are simply
-	// discarded. With this option, they are resolved alongside the irreducible
-	// clauses.
-	//   * Only use with proper limits (e.g. 'max_eliminations'). Otherwise,
-	//     this can produce a vast number of additional clauses because
-	//     reducible resolvents are not counted towards the 'growth' limit.
-	//   * TODO: a compromise where only some reducible resolvents are kept
-	bool resolve_reducible = false;
+	// Maximum size of reducible resolvents.
+	//   * Classic SatELite/Minisat just discards all reducible resolvents when
+	//     eliminating a variable. We instead resolve them along with the
+	//     irreducible ones, but only keep them if they are particularly short.
+	//   * Make sure to set proper limits (e.g. 'max_eliminations') if you want
+	//     to be generous with these. Otherwise, this can produce a vast number
+	//     of additional clauses because reducible resolvents are not counted
+	//     towards the 'growth' limit.
+	//   * TODO: increase the default a bit if/when we get better
+	//     otf-subsumption. Should probably be in line with the 'green cutoff'
+	//     in the CDCL searcher.
+	int green_cutoff = 3;
 
 	// maximum number of variables to eliminate
 	int64_t max_eliminations = INT64_MAX;
 
 	// maximum number of resolvents to generate before stopping
-	//   * reducible resolvents are counted towards this limit
-	int64_t max_resolvents = INT64_MAX;
+	//   * reducible resolvents are counted towards this limit (assuming they
+	//     pass the '.green_cutoff' limit)
+	int64_t max_resolvents = 20'000;
 };
 
 // returns number of removed variables
